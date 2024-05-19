943158d01f9d5f9428c8eb7cfae442d10464dc02
Vikas Sharma <you@example.com>
2024-05-20 00:59:53 +0530

    Css Add

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,27 +1,28 @@
-import {   useNavigate } from 'react-router-dom'
-import './profile.css'
-import {UserContext} from '../../lib/context/UserContext';
-import { useContext } from 'react';
-
+import { useNavigate } from "react-router-dom";
+import "./profile.css";
+import { UserContext } from "../../lib/context/UserContext";
+import { useContext } from "react";
 
 function Profile() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { user, updateUserState } = useContext(UserContext);
-  console.log("ProfileData",user)
-const logout = (e:any) => 
-  {e.preventDefault();
-    navigate("/signin")
-};
+  console.log("ProfileData", user);
+  const logout = (e: any) => {
+    e.preventDefault();
+    navigate("/signin");
+  };
   return (
     <div>
-      <div className='displayLogout'>
-          <button className="btn btn-primary"onClick={(e)=>logout(e)}>Log Out</button>
+      <div className="d-flex justify-content-end p-4">
+        <button className="btn btn-primary" onClick={(e) => logout(e)}>
+          Log Out
+        </button>
       </div>
-      <div className='p-4'>
-        <h1>Profile {user.name}</h1>
+      <div className="p-4">
+        <h1 className="text-center">Profile {user.name}</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,31 +1,31 @@
-import { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import {UserContext} from '../../lib/context/UserContext';
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../lib/context/UserContext";
 
 function SignUp() {
   const navigate = useNavigate();
   const [data, setData] = useState<UserModel>({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
     isUser: false,
     loggedIn: false,
   });
   const { user, updateUserState } = useContext(UserContext);
-  const [error, setError] = useState<  { message: string | null }>(
-    { message: null }
-  );
-  const handleSave = (e:any) => {
+  const [error, setError] = useState<{ message: string | null }>({
+    message: null,
+  });
+  const handleSave = (e: any) => {
     e.preventDefault();
 
     if (!data.name || !data.email || !data.password || !data.confirmPassword) {
-      setError({ message: 'All fields are required' });
+      setError({ message: "All fields are required" });
       return;
     }
 
     if (data.password !== data.confirmPassword) {
-      setError({ message: 'Password and Confirm Password do not match' });
+      setError({ message: "Password and Confirm Password do not match" });
       return;
     }
 
@@ -34,57 +34,64 @@ function SignUp() {
       email: data.email,
       password: data.password,
       isUser: true,
-      loggedIn: false
+      loggedIn: false,
     });
-    navigate('/signin');
+    navigate("/signin");
   };
 
   return (
-    <div>
-      <h1 className='m-4'>Sign Up</h1>
-      <form>
-        <label>Name *</label>
-        <div className='p-2'>
-          <input
-            type="text"
-            placeholder='Name'
-            onChange={(e) => setData({ ...data, name: e.target.value })}
-          >
-          </input>
-        </div>
-        <label>Email Id *</label>
-        <div className='p-2'>
-          <input
-            type="text"
-            placeholder='Email Id'
-            onChange={(e) => setData({ ...data, email: e.target.value })}
-          >
-          </input>
-        </div>
-        <label>Password *</label>
-        <div className='p-2'>
-          <input
-            type="password"
-            placeholder='Password'
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-          >
-          </input>
-        </div>
-        <label>Confirm Password *</label>
-        <div className='p-2'> 
-          <input
-            type="password"
-            placeholder='Confirm Password'
-            onChange={(e) => setData({ ...data, confirmPassword: e.target.value })}
-          >
-          </input>
-        </div>
-        {error.message && <p className='text-danger'>{error.message}</p>}
-        <div>
-          <Link to="/signIn">Already have an account? Sign in</Link>
-        </div>
-        <button className="btn btn-primary m-4" onClick={handleSave}>Sign Up</button>
-      </form>
+    <div className="d-flex justify-content-center">
+      <div className="bg-light bg-gradient p-5 border border-info m-4">
+        <h1 className="text-center mb-4">Sign Up</h1>
+        
+        <form>
+          <div className="mb-3">
+          <label className="form-label">Name *</label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Name"
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+            ></input>
+          </div>
+          <div className="mb-3">
+          <label>Email Id *</label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Email Id"
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+            ></input>
+          </div>
+          <div className="mb-3">
+          <label>Password *</label>
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setData({ ...data, password: e.target.value })}
+            ></input>
+          </div>
+          <div className="mb-3">
+          <label>Confirm Password *</label>
+            <input
+              className="form-control"
+              type="password"
+              placeholder="Confirm Password"
+              onChange={(e) =>
+                setData({ ...data, confirmPassword: e.target.value })
+              }
+            ></input>
+          </div>
+          {error.message && <p className="text-danger">{error.message}</p>}
+          <div  className="mb-3">
+            <Link to="/signIn">Already have an account? Sign in</Link>
+          </div>
+          <button className="btn btn-primary" onClick={handleSave}>
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
